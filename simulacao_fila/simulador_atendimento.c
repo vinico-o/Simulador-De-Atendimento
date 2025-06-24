32c391ccf2e498d623cf9c51546153c5c4f9db4b
RomeroNog <igorromeronigueira@gmail.com>
2025-06-24 10:50:26 -0300

    fix: alteração nas estatiscas de fila e adição de comentários

--- simulacao_fila/simulador_atendimento.c
+++ simulacao_fila/simulador_atendimento.c
@@ -19,6 +19,13 @@
 typedef struct{
     Bairro bairro;
     Fila servicos[NUM_SERVICOS];
+
+    //Estatisticas referentes a fila
+    int tempoEsperaFila[NUM_SERVICOS];
+    int num_pessoas_fila[NUM_SERVICOS];
+    int atendimentos_por_fila[NUM_SERVICOS];
+    int tempChegadaFila[NUM_SERVICOS];
+    int tempSaidaFila[NUM_SERVICOS];
 }BairroSimulacao;
 
 
@@ -128,46 +135,40 @@ int main()
 
     CidadaoSimulacao pessoa[NUM_PESSOAS];
     BairroSimulacao bairro[MAXBAIRRO];
-    int tempoEsperaFila[NUM_SERVICOS];
-
-    int num_pessoas_fila[NUM_SERVICOS];
-
-    int atendimentos_por_fila[NUM_SERVICOS];
-
-    int tempChegadaFila[NUM_SERVICOS];
-    int tempSaidaFila[NUM_SERVICOS];
-
     Pilha historico_atendimento[NUM_SERVICOS];
     noArvBB *ArvOcorrencia;
-    inicializanoArvBB(&ArvOcorrencia);
-
-    for(int i=0;i<NUM_SERVICOS;i++)
-    {
-        //Instruções para Fila
-        num_pessoas_fila[i] = 0;
-        atendimentos_por_fila[i] = 0;
-        tempoEsperaFila[i] = 0;
-
-        //Instruções para Pilha
-        inicializa_pilha(&historico_atendimento[i]);
-    }
-    for(int i=0;i<MAXBAIRRO;i++)
-    {
-        // inicializando cada bairro
-        for(int j=0;j<NUM_SERVICOS;j++)
-        {
-            inicializar(&bairro[i].servicos[j]);
-        }
-        bairro->bairro.id = i+1;
-        strcpy(bairro->bairro.nome,nomesBairros[i-1]);
-    }
     tabelaBairros tabelaBairro;
     tabelaCidadaos tabelaCidadao;
     TabHashUnidade TabelaUnidade;
     Unidade unidadeTemp;
+
+    //Inicalizando BST e tabelas hash
+    inicializanoArvBB(&ArvOcorrencia);
     InicializarTabelaHashUnidades(&TabelaUnidade);
     inicializarTabelaBairros (&tabelaBairro);
     inicializarTabelaCidadaos (&tabelaCidadao);
+
+    //Instruções para Pilha
+    for(int i=0;i<NUM_SERVICOS;i++)
+    {
+        inicializa_pilha(&historico_atendimento[i]);
+    }
+    // inicializando cada bairro
+    for(int i=0;i<MAXBAIRRO;i++)
+    {
+        for(int j=0;j<NUM_SERVICOS;j++)
+        {
+            inicializar(&bairro[i].servicos[j]);
+            bairro[i].atendimentos_por_fila[j] = 0;
+            bairro[i].num_pessoas_fila[j] = 0;
+            bairro[i].tempoEsperaFila[j] = 0;
+            bairro[i].tempChegadaFila[j] = 0;
+            bairro[i].tempSaidaFila[j] = 0;
+
+        }
+        bairro[i].bairro.id = i+1;
+        strcpy(bairro[i].bairro.nome,nomesBairros[i-1]);
+    }
 
     int idUnidade = 1;
 
@@ -178,7 +179,7 @@ int main()
     inserirTabelaBairros ("Rosas",&tabelaBairro);
     inserirTabelaBairros ("Aviacao",&tabelaBairro);
 
-
+    //Inicializando pessoas
     for(int i=0;i<NUM_PESSOAS;i++,num_cpf++)
     {
 
@@ -189,6 +190,8 @@ int main()
         pessoa[i].cidadao = malloc(sizeof(Cidadao));
         pessoa[i].cidadao->id = i+1;
         pessoa[i].servico_desejado = rand()% NUM_SERVICOS + 1; //gera aleatoriamente o serviço de cada pessoa
+        pessoa[i].cidadao->cpf = num_cpf;
+        pessoa[i].idOcorrencia = i+1;
     
 
         // aleatoriza um numero para escolher o nome do bairro
@@ -196,70 +199,95 @@ int main()
 
         // linka o bairro da tabela hash com o bairro do cidadao
         pessoa[i].cidadao->endereco = buscarBairro(nomesBairros[bairroAleatorio], &tabelaBairro);
-
         
-        pessoa[i].cidadao->cpf = num_cpf;
-        GerarOcorrenciaAleatorio(&pessoa[i]);
-        pessoa[i].idOcorrencia = i+1;
+        //Gerando nomes,email e ocorrencias para cada pessoa
         int indicetempNome;
         GerarNomeAleatorio(&pessoa[i],&indicetempNome);
         GerarEmailAleatorio(&pessoa[i],indicetempNome);
+        GerarOcorrenciaAleatorio(&pessoa[i]);
 
-        
         // inserindo na tabela
         inserirTabelaCidadaos(pessoa[i].cidadao, &tabelaCidadao);
-
-        
     }
 
-
+    //aletorizando a chegada e a saida
     chegada += rand()%2 + 1;
-    tempAtendimento += chegada + rand()%5 + 1;
+    tempAtendimento += chegada + rand()%16 + 1;
 
     while(tempTotal)
     {
+        //Quando a chegada acontece
+        //No if a pessoas que tem um bairro é inserida em uma das filas do seu bairro com base na ocorrência
         if(chegada == tempAtual && num_pessoa < NUM_PESSOAS)
         {
+            //Qual fila vai ser inserido
             int fila_inserido = pessoa[num_pessoa].servico_desejado - 1;
+            //Pega o id do bairro da pessoa para inserir na fila do bairro
             int idTempBairro = pessoa[num_pessoa].cidadao->endereco->id;
+            //Insere na fila respectiva
             inserir(&bairro[idTempBairro].servicos[fila_inserido],num_pessoa);
+            //Atualiza os dados da pessoa referentes a tempo na fila e em qual fila ela estava
             pessoa[num_pessoa].chegada = tempAtual;
             pessoa[num_pessoa].num_fila = fila_inserido;
+            bairro[idTempBairro].num_pessoas_fila[fila_inserido]++;
+            //Incrementa para a proxima pessoa
             num_pessoa++;
-            num_pessoas_fila[fila_inserido]++;
+            //Gera o tempo da nova chegada
             chegada += rand()%2 + 1;
 
         }
+        //If para a saida da fila
+        //Aqui ele tira uma pessoa de cada fila de cada bairro
         if(tempAtendimento == tempAtual)
         {
+            //Laço referente ao bairro (são 5)
             for(int j=0;j<MAXBAIRRO;j++)
             {
+                //Laço referente a cada fila de um bairro j
                 for(int i=0;i<NUM_SERVICOS;i++)
                 {
+                    //Caso a fila não esteja vazia ela será atendida. Sendo uma por vez de cada fila
                     if(!vazia(&bairro[j].servicos[i]))
                     {
+                        //Pega o numero da pessoa para poder analisar seus dados referentes ao atendimento
                         int pessoa_;
+                        //remove a pessoa de uma fila especifica de um bairro j
                         remover(&bairro[j].servicos[i],&pessoa_);
-    
+                        
+                        //Comando referentes a inserção da unidade utilizada na tabela hash de Unidades
                         unidadeTemp.id = idUnidade++;
                         strcpy(unidadeTemp.nome,nomesUnidades[pessoa[pessoa_].servico_desejado-1]);
                         InserirTabelaUnidades(unidadeTemp.id, unidadeTemp.nome, &TabelaUnidade);
-    
+                        
+                        //Print dos dados da pessoa que foi retirada de sua fila
                         printf("\tPessoa %d atendida!\n",pessoa_);
                         printf("\tcpf: %d \tnome: %s \tocorrencia: %s \temail:%s\n",pessoa[pessoa_].cidadao->cpf,pessoa[pessoa_].cidadao->nome,pessoa[pessoa_].ocorrencia,pessoa[pessoa_].cidadao->email);
-                        printf("\tOcorrencia adicionada ao historico de atendimento\n\n");
-                        inserirnoArvBB(&ArvOcorrencia,pessoa[pessoa_].idOcorrencia);
+                        
+                        //Cada pessoa tem um id referente a sua ocorrencia, a partir dele conseguimos recuperar a pessoa referente a esse id, como tambem sua ocorrência
+                        //Adiciona esse id em um BST
                         printf("ID da ocorrencia adicionada na BST!\n");
+                        inserirnoArvBB(&ArvOcorrencia,pessoa[pessoa_].idOcorrencia);
+                        
+                        //Adiciona cada ocorrencia referente a pilha referente a unidade solicitada
+                        //A pilha seria unica para cada unidade e não em relaçao ao bairro
+                        printf("\tOcorrencia adicionada ao historico de atendimento\n\n");
                         push(&historico_atendimento[i],pessoa[pessoa_].ocorrencia);
+
+                        //Dado para saber o tempo de espera da pessoa na fila
                         pessoa[pessoa_].saida = tempAtual;
-                        atendimentos_por_fila[i]++;
+
+                        //Contabiliza o número de atendimentos por fila
+                        bairro[j].atendimentos_por_fila[i]++;
                     }
                 }
             }
-            tempAtendimento += rand()%5 + 1;
+            //Gera um novo tempo para atendimento
+            tempAtendimento += rand()%16 + 1;
         }
 
+
         tempTotal--;
+        //Printa as filas de cada tempo
         printf("Tempo %d\n",tempAtual);
         for(int i=0;i<MAXBAIRRO;i++)
         {
@@ -274,10 +302,10 @@ int main()
         tempAtual++;
     }
     printf("Pessoas que tiveram ocorrencias: \n");
-    imprimirEmOrdem(&ArvOcorrencia);
-    ImprimirTabelaHashUnidades(&TabelaUnidade);
-    imprimirTabelaDeBairros(&tabelaBairro);
-    imprimirTabelaCidadaos (&tabelaCidadao);
+    //imprimirEmOrdem(&ArvOcorrencia);
+    //ImprimirTabelaHashUnidades(&TabelaUnidade);
+    //imprimirTabelaDeBairros(&tabelaBairro);
+    //imprimirTabelaCidadaos (&tabelaCidadao);
 
     
     return 0;
